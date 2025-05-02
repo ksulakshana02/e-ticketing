ea7f48164419d12457d519ebe430f5716fdf9f9b
ksulakshana02 <sulakshanakaveesha@gmail.com>
2025-05-02 16:27:55 +0530

    test

--- src/components/TranslateWrapper.tsx
+++ src/components/TranslateWrapper.tsx
@@ -1,31 +1,3 @@
-// "use client"
-// import {motion} from "framer-motion";
-// import {PropsWithChildren} from "react";
-//
-// interface Props extends PropsWithChildren {
-//     reverse?: boolean;
-//     duration?: number;
-// }
-//
-// const TranslateWrapper = ({children, reverse, duration}: Props) => {
-//     return (
-//         <motion.div
-//             initial={{translateX: reverse ? "-100%" : "0%"}}
-//             animate={{translateX: reverse ? "0%" : "-100%"}}
-//             transition={{
-//                 duration: duration ?? 20,
-//                 repeat: Infinity,
-//                 ease: "linear",
-//             }}
-//             className="flex gap-4 mr-4"
-//         >
-//             {children}
-//         </motion.div>
-//     );
-// };
-//
-// export default TranslateWrapper;
-
 "use client"
 import {motion} from "framer-motion";
 import {PropsWithChildren} from "react";
